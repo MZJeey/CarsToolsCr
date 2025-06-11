3120025a36e133cfb70f6465d1ff98bcdaa34227
FranciscoDavila09 <abernydavila16@gmail.com>
2025-06-11 17:25:47 -0600

    conflictos resueltos en EtiquetaController

--- models/EtiquetaModel.php
+++ models/EtiquetaModel.php
@@ -5,79 +5,89 @@ class EtiquetaModel
     public function __construct()
     {
         $this->enlace = new MySqlConnect();
+
+}
+// listar todas las etiquetas
+
+
+public function all()
+{
+    try {
+        //Consulta SQL
+        $vSQL = "SELECT * FROM Etiqueta";
+        $vResultado = $this->enlace->ExecuteSQL($vSQL);
+        return $vResultado;
+    } catch (Exception $e) {
+        handleException($e);
     }
-    // listar todas las etiquetas
+}
+//obetener una etiqueta por ID
 
+   public function get($id)
+{
+    try {
 
-    public function all()
-    {
-        try {
-            //Consulta SQL
-            $vSQL = "SELECT * FROM Etiqueta";
-            $vResultado = $this->enlace->ExecuteSQL($vSQL);
-            return $vResultado;
-        } catch (Exception $e) {
-            handleException($e);
+        $sql = "SELECT * FROM etiqueta WHERE id = :id";
+        $resultado= $this->enlace->executeSQL($sql);
+        return $resultado[0] ?? null;
+
+  }catch (Exception $e) {
+       handleException($e);
+
+    }
+}
+ // Crear una nueva etiqueta
+ public function create($data)
+ {
+    try {
+        $sql= "INSERT INTO etiqueta (nombre) VALUES ('{$data['nombre']}')";
+    return $this->enlace->executeSQL_DML( $sql);
+    } catch (Exception $e) {
+             handleException($e);
+
         }
-    }
-    //obetener una etiqueta por ID
+ }
 
-    public function get($id)
-    {
-        try {
+ // Actualizar una etiqueta 
 
-            $sql = "SELECT * FROM etiqueta WHERE id = :id";
-            $resultado = $this->enlace->executeSQL($sql);
-            return $resultado[0] ?? null;
-        } catch (Exception $e) {
-            handleException($e);
-        }
-    }
-    // Crear una nueva etiqueta
-    public function create($data)
-    {
-        try {
-            $sql = "INSERT INTO Etiqueta (nombre) VALUES ('{$data['nombre']}')";
-            return $this->enlace->executeSQL_DML($sql);
-        } catch (Exception $e) {
-            handleException($e);
-        }
-    }
-
-    // Actualizar una etiqueta 
-
-    public function update($id, $data)
-    {
-        try {
+ public function update($id, $data)
+ {
+    try {
             $sql = "UPDATE etiqueta SET nombre = '{$data['nombre']}' WHERE id = $id";
             return $this->enlace->executeSQL_DML($sql);
         } catch (Exception $e) {
             handleException($e);
         }
+ }
+// Eliminar etiqueta 
+public function delete($id)
+{
+    try{
+        $sql= "DELETE FROM etiqueta WHERE id = $id";
+        return $this->enlace->executeSQL_DML($sql);
+    }catch (Exception $e) {
+        handleException($e);
+
     }
-    // Eliminar etiqueta 
-    public function delete($id)
-    {
-        try {
-            $sql = "DELETE FROM etiqueta WHERE id = $id";
-            return $this->enlace->executeSQL_DML($sql);
-        } catch (Exception $e) {
-            handleException($e);
-        }
-    }
+}
 
 
-    // Obtener todas las etiquetas de un producto específico
-    public function getByProducto($producto_id)
-    {
-        try {
-            $sql = "SELECT e.*
+// Obtener todas las etiquetas de un producto específico
+public function getByProducto($producto_id)
+{
+    try {
+        $sql = "SELECT e.*
                 FROM etiqueta e
                 JOIN productoetiqueta pe ON e.id = pe.etiqueta_id
                 WHERE pe.producto_id = $producto_id";
-            return $this->enlace->executeSQL($sql);
-        } catch (Exception $e) {
-            handleException($e);
-        }
+        return $this->enlace->executeSQL($sql);
+    } catch (Exception $e) {
+        handleException($e);
     }
+}
+
+
+
+
+
 }
