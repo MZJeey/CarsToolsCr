16d2aa289f679a94ff53dfd1f1cd28abffd78581
FranciscoDavila09 <abernydavila16@gmail.com>
2025-06-20 16:29:26 -0600

    Se creo tanto el controller de promocion como el model ya revisados en postman

---
+++ models/PromocionModel.php
@@ -0,0 +1,89 @@
+<?php
+class PromocionModel
+{
+    private $enlace;
+
+      public function __construct()
+      {
+        $this->enlace = new MySqlConnect();
+      }
+
+      public function all()
+      {
+         try {
+            $sql = "SELECT * FROM promocion";
+            return $this->enlace->executeSQL($sql);
+        } catch (Exception $e) {
+            handleException($e);
+        }
+      }
+
+   public function get($id)
+    {
+        try {
+            $sql = "SELECT * FROM promocion WHERE id = $id";
+            $result = $this->enlace->executeSQL($sql);
+            return !empty($result) ? $result[0] : null;
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+
+    public function create($data)
+    {
+        try {
+            $sql = "INSERT INTO promocion (tipo, objetivo_id, descuento, fecha_inicio, fecha_fin) VALUES (
+                '{$data['tipo']}',
+                {$data['objetivo_id']},
+                {$data['descuento']},
+                '{$data['fecha_inicio']}',
+                '{$data['fecha_fin']}'
+            )";
+            return $this->enlace->executeSQL_DML_last($sql);
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+
+    public function update($id, $data)
+    {
+        try {
+            $sql = "UPDATE promocion SET
+                tipo = '{$data['tipo']}',
+                objetivo_id = {$data['objetivo_id']},
+                descuento = {$data['descuento']},
+                fecha_inicio = '{$data['fecha_inicio']}',
+                fecha_fin = '{$data['fecha_fin']}'
+                WHERE id = $id";
+            return $this->enlace->executeSQL_DML($sql);
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $sql = "DELETE FROM promocion WHERE id = $id";
+            return $this->enlace->executeSQL_DML($sql);
+        } catch (Exception $e) {
+            handleException($e);
+        }
+    }
+
+
+public function getActivas()
+{
+    try {
+        $sql = "SELECT * FROM promocion 
+                WHERE NOW() BETWEEN fecha_inicio AND fecha_fin";
+        return $this->enlace->executeSQL($sql);
+    } catch (Exception $e) {
+        handleException($e);
+    }
+}
+
+
+
+
+}
