83fbc0b6f4de735d9061f19c03b4558f15fb288f
Jeeyson <jasonezezel@gmail.com>
2025-07-26 09:37:50 -0600

    Revert "Merge branch 'developer' into berny"
    This reverts commit 306b7fc22f4356f1f33af1ccc76a8617e1e4d91a, reversing
    changes made to fec49c5720bdaf19fa3692db05f54bcfff85c758.

--- appShopTools/src/components/Home/Home.jsx
+++ appShopTools/src/components/Home/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
 import MuiCarousel from "../Productos/carrusel";
-
 export function Home() {
   return (
-    <Container sx={{ pt: 2, pb: 2, mt: 0 }} maxWidth="md">
-      <MuiCarousel sx={{ maxWidth: "100%" }} />
+    <Container sx={{ p: 2 }} maxWidth="sm">
+      <Typography
+        component="h1"
+        variant="h2"
+        align="center"
+        color="text.primary"
+        gutterBottom
+      >
+        CarsToolsCr
+      </Typography>
+      <Typography variant="h5" align="center" color="text.secondary">
+        Todo lo que tu vehiculo necesita hasta la puerta de tu garaje.
+      </Typography>
+      <MuiCarousel />
     </Container>
   );
 }
