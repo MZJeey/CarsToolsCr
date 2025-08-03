07b163da4bdd50ff940055ceca66d08c86029add
Jeeyson <jasonezezel@gmail.com>
2025-08-03 16:33:07 -0600

    Domingo para merge

--- routes/RoutesController.php
+++ routes/RoutesController.php
@@ -15,7 +15,7 @@ class RoutesController
     {
         // Registrar rutas protegidas
         //---------------------  Metodo,path (en minuscula),controlador, accion, array de nombres de roles
-        $this->addProtectedRoute('GET', '/appShopTools/user', 'user', 'index', ['Administrador']);
+        $this->addProtectedRoute('GET', '/apimovie/actor', 'actor', 'index', ['Administrador']);
     }
 
     public function routes()
